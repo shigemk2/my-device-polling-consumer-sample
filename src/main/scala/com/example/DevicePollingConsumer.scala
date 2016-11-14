74079080d538d33eeadeccb53be8ea1e0380164f
shigemk2 <i.am.shige@gmail.com>
2016-11-15 00:24:07 +0900

    Add EvenNumberDevice and Timeout

--- src/main/scala/com/example/DevicePollingConsumer.scala
+++ src/main/scala/com/example/DevicePollingConsumer.scala
@@ -1,8 +1,44 @@
 package com.example
 
+import java.util.Date
+
 import akka.actor._
+
+import scala.util.Random
 
 object DevicePollingConsumerDriver extends CompletableApp(10) {
 }
 
 case class Monitor()
+
+class EvenNumberDevice() {
+  val random = new Random(99999)
+
+  def nextEvenNumber(waitFor: Int): Option[Int] = {
+    val timeout = new Timeout(waitFor)
+    var nextEvenNumber: Option[Int] = None
+
+    while (!timeout.isTimedOut && nextEvenNumber.isEmpty) {
+      Thread.sleep(waitFor / 2)
+
+      val number = random.nextInt(100000)
+
+      if (number % 2 == 0) nextEvenNumber = Option(number)
+    }
+
+    nextEvenNumber
+  }
+}
+
+class Timeout(withinMillis: Int) {
+  val mark = currentTime
+
+  def isTimedOut(): Boolean = {
+    if (withinMillis == -1) false
+    else currentTime - mark >= withinMillis
+  }
+
+  private def currentTime(): Long = {
+    new Date().getTime
+  }
+}
